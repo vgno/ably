df83278298314fd9ae4193b039f0f7198d207836
Wadim Grasza <wadim.grasza@gmail.com>
2015-03-26 22:39:50 +0100

    Reformat test descriptions

--- test/ably.test.js
+++ test/ably.test.js
@@ -4,35 +4,37 @@ var ably = require('../ably.js');
 describe('Ably', function() {
     'use strict';
 
-    it('should have a addTest method', function() {
+    it('has an addTest method', function() {
         assert.equal(typeof ably, 'object');
         assert.equal(typeof ably.addTest, 'function');
     });
 
-    it('should have a addTests method', function() {
+    it('has an addTests method', function() {
         assert.equal(typeof ably, 'object');
         assert.equal(typeof ably.addTests, 'function');
     });
 
-    it('should have a getTests method', function() {
+    it('has a getTests method', function() {
         assert.equal(typeof ably, 'object');
         assert.equal(typeof ably.getTests, 'function');
     });
 
-    it('should have a when method', function() {
+    it('has a when method', function() {
         assert.equal(typeof ably, 'object');
         assert.equal(typeof ably.when, 'function');
     });
 
-    it('addTest() should add tests retrievable by getTests()', function() {
-        var tests = [
-            {name:'button-color'},
-            {name:'button-text'}
-            ];
+    describe('.addTest()', function() {
+        it('adds tests retrievable by getTests()', function() {
+            var tests = [
+                {name:'button-color'},
+                {name:'button-text'}
+                ];
 
-        ably.addTest(tests[0]);
-        ably.addTest(tests[1]);
+            ably.addTest(tests[0]);
+            ably.addTest(tests[1]);
 
-        assert.deepEqual(ably.getTests(), tests);
+            assert.deepEqual(ably.getTests(), tests);
+        });
     });
 });
