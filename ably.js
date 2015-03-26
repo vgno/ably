11337c044cd68a8a89b20187e78728b09010fa08
Wadim Grasza <wadim.grasza@gmail.com>
2015-03-26 21:10:53 +0100

    Add an exports-type module definition

--- ably.js
+++ ably.js
@@ -3,6 +3,11 @@
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
     } else {
         // Browser globals (root is window)
         root.ably = factory();
